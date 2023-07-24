889921a638c271c22aa12721c6d9cde470f8570b
Tomas Pennisi Moreira <moreiratomas22@gmail.com>
2023-07-24 12:04:31 -0300

    Clase 3, creando componentes

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const NavBar = () => {
+    return (
+    <div>
+        <nav id="navItems">
+            <li>Inicio</li>
+            <li>Productos</li>
+            <li>PRUEBA</li>
+            <li>Nosotros</li>
+        
+        </nav>
+       
+    </div>
+    )
+}
+
+export default NavBar
